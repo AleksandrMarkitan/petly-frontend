28a0479ef80b067188a7b9ef9fe8ce3e8d282714
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-01-13 17:45:06 +0200

    made test routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,57 @@
-// import { Routes, Route } from 'react-router-dom';
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-// import { PublicRoute } from 'HOCs/PublicRoute';
-// import { PrivateRoute } from 'HOCs/PrivateRoute';
+import { PublicRoute } from "HOCs/PublicRoute";
+import { PrivateRoute } from "HOCs/PrivateRoute";
 
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route
+            path="register"
+            element={
+              <PublicRoute>
+                <RegisterPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="friends"
+            element={
+              <PublicRoute>
+                <OurFriendsPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="news"
+            element={
+              <PublicRoute>
+                <NewsPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="user"
+            element={
+              <PrivateRoute>
+                <UserPage />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
+  );
 };
