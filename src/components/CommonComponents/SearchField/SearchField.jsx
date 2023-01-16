e52fbadfd134153f787fa289fb3b59ea5f357ae7
Anna <98224289+AnyaNass@users.noreply.github.com>
2023-01-16 12:30:32 +0200

    search field is done

---
+++ src/components/CommonComponents/SearchField/SearchField.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { InputSearch, LabelSearch, RemoveBtn } from "./SearchField.styled";
+import { ReactComponent as SearchIcon } from "../../../icons/search-icon.svg";
+import { ReactComponent as RemoveIcon } from "../../../icons/remove-search-query-icon.svg";
+
+export const SearchField = () => {
+	const [searchQuery, setSearchQuery] = useState('');
+
+	const searchHandler = e => {
+		setSearchQuery(e.target.value);
+	}
+
+	const removeQuery = () => {
+		setSearchQuery("")
+	}
+
+	return <LabelSearch>
+		<InputSearch type="search" placeholder="Search" value={searchQuery} onChange={searchHandler} />
+		{!searchQuery && <SearchIcon />}
+		{searchQuery && <RemoveBtn type="button" onClick={removeQuery}><RemoveIcon /></RemoveBtn>}
+	</LabelSearch>
+}
