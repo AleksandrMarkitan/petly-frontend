acb5a4952515e7bc68e4ce2ed2672c7dca82b389
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-01-18 01:14:37 +0200

    add layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router";
 import { Header } from "../Header/Header";
 
 export const Layout = () => {
-	return (
-		<>
-			<Header />
-			<Outlet />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 };
