88679a7492d54ec5d4921c038a80cfb5b7e3b546
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-01-18 01:09:17 +0200

    add themeProvider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,53 +6,61 @@ import { PublicRoute } from "../HOCs/PublicRoute";
 import { PrivateRoute } from "../HOCs/PrivateRoute";
 
 export const App = () => {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route
-						index
-						path="register"
-						element={
-							<PublicRoute redirectTo="/" restricted>
-								<RegisterPage />
-							</PublicRoute>
-						}
-					/>
-					<Route
-						path="login"
-						element={
-							<PublicRoute redirectTo="/" restricted>
-								<LoginPage />
-							</PublicRoute>
-						}
-					/>
-					<Route
-						path="friends"
-						element={
-							<PublicRoute redirectTo="/" restricted>
-								<OurFriendsPage />
-							</PublicRoute>
-						}
-					/>
-					<Route
-						path="news"
-						element={
-							<PublicRoute redirectTo="/" restricted>
-								<NewsPage />
-							</PublicRoute>
-						}
-					/>
-					<Route
-						path="user"
-						element={
-							<PrivateRoute redirectTo="/login">
-								<UserPage />
-							</PrivateRoute>
-						}
-					/>
-				</Route>
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            path="register"
+            element={
+              <PublicRoute redirectTo="/" restricted>
+                <RegisterPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute redirectTo="/" restricted>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="friends"
+            element={
+              <PublicRoute redirectTo="/" restricted>
+                <OurFriendsPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="news"
+            element={
+              <PublicRoute redirectTo="/" restricted>
+                <NewsPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="user"
+            element={
+              <PublicRoute redirectTo="/" restricted>
+                <UserPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="notices/:categoryName"
+            element={
+              <PrivateRoute redirectTo="/login">
+                <NoticesPage />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
+  );
 };
