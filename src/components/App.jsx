de25320351d84be175322b43d3ee7b6ee6c00bbd
Anna <98224289+AnyaNass@users.noreply.github.com>
2023-01-17 12:19:51 +0200

    added a message to news page, if any news wasn`t found

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,58 +2,60 @@ import { Routes, Route } from "react-router-dom";
 // import { useEffect, Suspense } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 
-// import { PublicRoute } from "HOCs/PublicRoute";
-// import { PrivateRoute } from "HOCs/PrivateRoute";
+import { PublicRoute } from "../HOCs/PublicRoute";
+// import { PrivateRoute } from "../HOCs/PrivateRoute";
+
 import { Layout } from "./Layout/Layout";
+import { NewsPage } from "../pages/NewsPage/NewsPage";
 
 export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* <Route
-            index
-            path="register"
-            element={
-              <PublicRoute redirectTo="/" restricted>
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <PublicRoute redirectTo="/" restricted>
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="friends"
-            element={
-              <PublicRoute redirectTo="/" restricted>
-                <OurFriendsPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="news"
-            element={
-              <PublicRoute redirectTo="/" restricted>
-                <NewsPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="user"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <UserPage />
-              </PrivateRoute>
-            }
-          /> */}
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					{/* <Route
+						index
+						path="register"
+						element={
+							<PublicRoute redirectTo="/" restricted>
+								<RegisterPage />
+							</PublicRoute>
+						}
+					/>
+					<Route
+						path="login"
+						element={
+							<PublicRoute redirectTo="/" restricted>
+								<LoginPage />
+							</PublicRoute>
+						}
+					/>
+					<Route
+						path="friends"
+						element={
+							<PublicRoute redirectTo="/" restricted>
+								<OurFriendsPage />
+							</PublicRoute>
+						}
+					/> */}
+					<Route
+						path="news"
+						element={
+							<PublicRoute redirectTo="/" restricted>
+								<NewsPage />
+							</PublicRoute>
+						}
+					/>
+					{/* <Route
+						path="user"
+						element={
+							<PrivateRoute redirectTo="/login">
+								<UserPage />
+							</PrivateRoute>
+						}
+					/> */}
+				</Route>
+			</Routes>
+		</>
+	);
 };
