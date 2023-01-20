e63e684bb64127742b42f633cb5369fe5db2b78f
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-01-20 14:36:16 +0200

    add notice basic routes structure and start test redux

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,3 +1,36 @@
+import { Suspense, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet, useParams } from "react-router-dom";
+
+import { fetchNotices } from "../../redux/notices/noticesOperations";
+
+import { SectionTitle } from "../../components/CommonComponents/SectionTitle/SectionTitle";
+import { SearchField } from "../../components/CommonComponents/SearchField/SearchField";
+import { NoticesCategoriesNav } from "../../components/NoticesCategoriesNav/NoticesCategoriesNav";
+import { Section } from "../../components/CommonComponents/Section/Section";
+import { Container } from "../../components/CommonComponents/Container/Container";
+// -----------------------------------------------------------
+
 export const NoticesPage = () => {
-  return <h1>NoticesPage</h1>;
+  const dispatch = useDispatch();
+
+  const { category } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchNotices({ category }));
+  }, [dispatch, category]);
+
+  return (
+    <Section>
+      <Container>
+        <SectionTitle text={"Find your favorite pet"} />
+        <SearchField />
+        <NoticesCategoriesNav />
+
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </Container>
+    </Section>
+  );
 };
