5f67da4314768637cc1473629ea14bb3c9a5987f
Anna <98224289+AnyaNass@users.noreply.github.com>
2023-01-20 16:55:20 +0200

    Update getNews.js

--- src/serveсes/getNews.js
+++ src/serveсes/getNews.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1"
+const BASE_URL = "http://localhost:4000/api/v1"
 
 export const getNews = async ({ page = 1, limit = 0 }) => {
 	const response = await fetch(`${BASE_URL}/news?page=${page}&limit=${limit}`);
