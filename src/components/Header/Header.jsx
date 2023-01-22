4912e195543469d95995d666fa3c9ec19c6f5ca1
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2023-01-22 20:22:53 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import { Container } from "../CommonComponents/Container/Container";
 import { Logo } from "../Logo/Logo";
-import { ModalNotice } from "../ModalNotice/ModalNotice";
 import { Navigation } from "../Navigation/Navigation";
 
 import { HeaderStyled, Div } from "./Header.styled";
@@ -13,7 +12,6 @@ export const Header = () => {
           <Logo />
           <Navigation />
         </Div>
-        <ModalNotice />
       </Container>
     </HeaderStyled>
   );
