2619512304d6e529e2d4c3eb408bfef68dd7abbf
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2023-01-22 21:46:13 +0200

    Update ModalNotice.jsx

--- src/components/ModalNotice/ModalNotice.jsx
+++ src/components/ModalNotice/ModalNotice.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { ModalWindow } from "../CommonComponents/ModalWindow/ModalWindow";
 import {
   Box,
   BoxBtns,
@@ -18,100 +16,91 @@ import {
 } from "./ModalNotice.styled";
 
 export const ModalNotice = ({
-  title,
-  category,
-  name,
-  birthdate,
-  breed,
-  sex,
-  location,
-  comments,
-  imgURL,
-  owner,
-  sell,
+  data: {
+    title,
+    category,
+    name,
+    birthdate,
+    breed,
+    sex,
+    location,
+    comments,
+    imgURL,
+    price,
+    owner: { phone, email },
+  },
 }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const closeModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
-  const hrefMail = `mailto:${owner}`;
-  const hrefTel = `tel:/* +380991111111 */${owner}`;
+  const hrefMail = `mailto:${email}`;
+  const hrefTel = `tel:${phone}`; /*Format: +380991111111 */
 
   return (
     <>
-      <button onClick={closeModal}>ВІДКРИТИ МОДАЛКУ</button>
-      {isModalOpen && (
-        <ModalWindow onClose={closeModal}>
-          <Box>
-            <ThumbImg>
-              <Img src={imgURL} alt={name} />
-              <Category>{category}</Category>
-            </ThumbImg>
+      <Box>
+        <ThumbImg>
+          <Img src={imgURL} alt={name} />
+          <Category>{category}</Category>
+        </ThumbImg>
 
-            <BoxTitleList>
-              <Title>{title}</Title>
+        <BoxTitleList>
+          <Title>{title}</Title>
 
-              <ListOfValues>
-                <BoxValue>
-                  <TitleValue>Name:</TitleValue>
-                  <Value>{name}</Value>
-                </BoxValue>
-                <BoxValue>
-                  <TitleValue>Birthday:</TitleValue>
-                  <Value>{birthdate}</Value>
-                </BoxValue>
-                <BoxValue>
-                  <TitleValue>Breed:</TitleValue>
-                  <Value>{breed}</Value>
-                </BoxValue>
-                <BoxValue>
-                  <TitleValue>Place:</TitleValue>
-                  <Value>{location}</Value>
-                </BoxValue>
-                <BoxValue>
-                  <TitleValue>The sex:</TitleValue>
-                  <Value>{sex}</Value>
-                </BoxValue>
-                <BoxValue>
-                  <TitleValue>Email:</TitleValue>
-                  <Value>
-                    <a href={hrefMail}>{}Email</a>
-                  </Value>
-                </BoxValue>
-                <BoxValue>
-                  <TitleValue>Phone:</TitleValue>
-                  <Value>
-                    <a href={hrefTel}>{}Phone</a>
-                  </Value>
-                </BoxValue>
-                {sell && (
-                  <BoxValue>
-                    <TitleValue>Sell:</TitleValue>
-                    <Value>{sell}</Value>
-                  </BoxValue>
-                )}
-              </ListOfValues>
-            </BoxTitleList>
-          </Box>
+          <ListOfValues>
+            <BoxValue>
+              <TitleValue>Name:</TitleValue>
+              <Value>{name}</Value>
+            </BoxValue>
+            <BoxValue>
+              <TitleValue>Birthday:</TitleValue>
+              <Value>{birthdate}</Value>
+            </BoxValue>
+            <BoxValue>
+              <TitleValue>Breed:</TitleValue>
+              <Value>{breed}</Value>
+            </BoxValue>
+            <BoxValue>
+              <TitleValue>Place:</TitleValue>
+              <Value>{location}</Value>
+            </BoxValue>
+            <BoxValue>
+              <TitleValue>The sex:</TitleValue>
+              <Value>{sex}</Value>
+            </BoxValue>
+            <BoxValue>
+              <TitleValue>Email:</TitleValue>
+              <Value>
+                <a href={hrefMail}>{email}</a>
+              </Value>
+            </BoxValue>
+            <BoxValue>
+              <TitleValue>Phone:</TitleValue>
+              <Value>
+                <a href={hrefTel}>{phone}</a>
+              </Value>
+            </BoxValue>
+            {price && (
+              <BoxValue>
+                <TitleValue>Sell:</TitleValue>
+                <Value>{price}</Value>
+              </BoxValue>
+            )}
+          </ListOfValues>
+        </BoxTitleList>
+      </Box>
 
-          <Comments>
-            <b>Comments: </b>
-            {comments}
-          </Comments>
+      <Comments>
+        <b>Comments: </b>
+        {comments}
+      </Comments>
 
-          <BoxBtns>
-            <Btn type="button">
-              Add to
-              <span>
-                <Heart />
-              </span>
-            </Btn>
-            <Btn type="button">Contact</Btn>
-          </BoxBtns>
-        </ModalWindow>
-      )}
+      <BoxBtns>
+        <Btn type="button">
+          Add to
+          <span>
+            <Heart />
+          </span>
+        </Btn>
+        <Btn type="button">Contact</Btn>
+      </BoxBtns>
     </>
   );
 };
