e64c6752bb1594ae9d83c61f6d845299058de2cf
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-24 13:29:09 +0200

    delete comments

--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -6,44 +6,12 @@ import { ModalAddPet } from '../ModalAddPet/ModalAddPet';
 import { AddTitle, BtnBox, BoxTitlePet } from './PetsData.styled';
 import { TitleUser } from '../UserData/UserData.styled';
 
-// import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
-// import * as Yup from "yup";
-// import { FieldsWrapper } from "./ModalAddPet.styled";
-// import { CancelBtn } from "../CommonButtons/CancelBtn/CancelBtn";
-// import { NextBtn } from "../CommonButtons/NextBtn/NextBtn";
-
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchUserPets } from "../../redux/user/userOperations";
-// import { selectUser } from "../../redux/user/userSelectors";
-
 export const PetsData = ({ pets }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  //   const [nextFormShow, setNextFormShow] = useState(false);
-  //   const [backFormShow, setBackFormShow] = useState(true);
   const closeModal = () => {
     setIsModalOpen(!isModalOpen);
   };
-  //   const pets = useSelector(selectUser);
-  //   const dispatch = useDispatch();
-  //   useEffect(() => {
-  //     dispatch(fetchUserPets());
-  //   }, [dispatch]);
-  //   console.log(pets);
-
-  /////////----------------------
-  //   const handleNext = () => {
-  //     setNextFormShow(true);
-  //     setBackFormShow(false);
-  //   };
-  //   const handleBack = () => {
-  //     setBackFormShow(false);
-  //     setBackFormShow(true);
-  //   };
-  //   const handleSubmit = () => {
-  //     console.log(777);
-  //   };
 
   return (
     <>
@@ -54,16 +22,11 @@ export const PetsData = ({ pets }) => {
           <AddBtn closeModal={closeModal} />
         </BtnBox>
       </BoxTitlePet>
-
       {isModalOpen && (
         <ModalWindow onClose={closeModal} modalType={'addPet'}>
           <ModalAddPet />
         </ModalWindow>
       )}
-      {/* <ModalAddPet /> */}
-      {/* <button onClick={closeModal}>ВІДКРИТИ МОДАЛКУ</button> */}
-      {/* {isModalOpen && <ModalAddPet />} */}
-
       {pets && <PetsList pets={pets} />}
     </>
   );
