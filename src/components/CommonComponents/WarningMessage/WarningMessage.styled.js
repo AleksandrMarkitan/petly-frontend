fc78d300b49cc6bc1b02f1e9ad2a1cf983276ca8
Anna <aaaananas1@gmail.com>
2023-01-29 18:46:09 +0200

    fix add pet modal and WarningMessage

--- src/components/CommonComponents/WarningMessage/WarningMessage.styled.js
+++ src/components/CommonComponents/WarningMessage/WarningMessage.styled.js
@@ -27,7 +27,7 @@ export const Text = styled.p`
 	}
 `
 
-export const LinkWrap = styled.div`
+export const Wrap = styled.div`
 	margin-top: 24px;
 	display: flex;
 	flex-direction: column;
@@ -42,6 +42,28 @@ export const LinkWrap = styled.div`
 
 export const StyledLink = styled(Link)`
 	text-align: center;
+	padding: 8px 0;
+	width: 100%;
+	font-size: 16px;
+	line-height: calc(22 / 16);
+	letter-spacing: 0.04em;
+	color: #FFFFFF;
+	background-color: #F59256;
+	border: 2px solid #F59256;
+	border-radius: 40px;
+	transition: color 300ms linear, background-color 300ms linear;
+
+	:hover, :focus{
+		color: #F59256;
+		background-color: #fff;
+	}
+
+	@media (min-width: 768px) {
+		width: 180px;
+	}
+`
+
+export const Button = styled.button`
 	padding: 8px 0;
 	width: 100%;
 	font-size: 16px;
