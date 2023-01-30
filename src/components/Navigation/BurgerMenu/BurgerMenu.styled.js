c3fa68f17367832146bb471956f0e1801ab42f23
Anna <aaaananas1@gmail.com>
2023-01-30 14:23:16 +0200

    fix mobile menu

--- src/components/Navigation/BurgerMenu/BurgerMenu.styled.js
+++ src/components/Navigation/BurgerMenu/BurgerMenu.styled.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 import { CgClose } from 'react-icons/cg';
 
 export const NavStyled = styled.nav`
-  position: absolute;
+  position: fixed;
   background-color: rgba(253, 247, 242, 1);
   z-index: 50;
   padding: 16px 20px;
   width: 100%;
+  height: 100%;
   top: 0;
-  right: 0;
+  left: 0;
   box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
-  border-radius: 20px;
 
   @media (min-width: 768px) {
     padding: 24px 32px;
