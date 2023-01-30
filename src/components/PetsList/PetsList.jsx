bc92dc9598c134f8aa00097646237de5beb4b004
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-30 14:24:31 +0200

    fix modal

--- src/components/PetsList/PetsList.jsx
+++ src/components/PetsList/PetsList.jsx
@@ -7,36 +7,44 @@ import { WrapperPicDiv, Box, Description, InfoPet } from './PetsList.styled';
 import { WarningMessage } from '../CommonComponents/WarningMessage/WarningMessage';
 
 export const PetsList = ({ pets }) => {
-	const dispatch = useDispatch();
-	const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	const closeModal = () => {
-		setIsModalOpen(!isModalOpen)
-	};
+  const closeModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-	const deletePetItem = id => {
-		dispatch(deletePet(id));
-	};
+  const deletePetItem = id => {
+    dispatch(deletePet(id));
+  };
 
-	return (
-		<>
-			{pets.map(({ _id, avatarURL, name, date, breed, comments }) => (
-				<Box key={_id}>
-					<WrapperPicDiv src={avatarURL} alt="pic" />
-					<Description>
-						<DeletePetBtn onClick={closeModal} />
-						<InfoPet>Name: {name}</InfoPet>
-						<InfoPet>Date of birth: {date}</InfoPet>
-						<InfoPet>Breed: {breed}</InfoPet>
-						<InfoPet>Comments: {comments}</InfoPet>
-					</Description>
-					{isModalOpen && <WarningMessage type="approve" id={_id} approveFunk={deletePetItem} onClose={closeModal} text="Are you sure than you want to delete the pet?" title="Approve" />}
-				</Box>
-			))}
-		</>
-	);
+  return (
+    <>
+      {pets.map(({ _id, avatarURL, name, date, breed, comments }) => (
+        <Box key={_id}>
+          <WrapperPicDiv src={avatarURL} alt="pic" />
+          <Description>
+            <DeletePetBtn onClick={closeModal} />
+            <InfoPet>Name: {name}</InfoPet>
+            <InfoPet>Date of birth: {date}</InfoPet>
+            <InfoPet>Breed: {breed}</InfoPet>
+            <InfoPet>Comments: {comments}</InfoPet>
+          </Description>
+          {isModalOpen && (
+            <WarningMessage
+              type="approve"
+              id={_id}
+              approveFunk={deletePetItem}
+              onClose={closeModal}
+              text="Are you sure than you want to delete the pet?"
+            />
+          )}
+        </Box>
+      ))}
+    </>
+  );
 };
 
 PetsList.propTypes = {
-	pets: PropTypes.array.isRequired,
+  pets: PropTypes.array.isRequired,
 };
